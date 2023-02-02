394b528a36651b46ea8f6c39172b6369f981b8ff
kolkul <kol9.kulinich@gmail.com>
2023-02-02 11:46:51 -0800

    Added layouts
    Also:
    - added router settings
    - created the first pages

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
+<script>
+import EmptyLayout from "@/layout/EmptyLayout";
+import MainLayout from "@/layout/MainLayout";
+
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return (this.$route.meta.layout || "Empty") + "Layout";
+    },
+  },
+  components: {
+    EmptyLayout,
+    MainLayout,
+  },
+};
+</script>
+
 <style>
 @import "~materialize-css/dist/css/materialize.min.css";
+@import "@/assets/index.css";
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
-]
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login"),
+    meta: { layout: "Empty" },
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    component: () => import("../views/Categories"),
+    meta: { layout: "Main" },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
